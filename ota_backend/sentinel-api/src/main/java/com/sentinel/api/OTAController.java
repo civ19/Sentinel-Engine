067a645ef09cc364960cada01409ab394486840f
Ronaldo <ronaldoanthony12@gmail.com>
2026-08-03 17:33:43 -0400

    Update check response

--- ota_backend/sentinel-api/src/main/java/com/sentinel/api/OTAController.java
+++ ota_backend/sentinel-api/src/main/java/com/sentinel/api/OTAController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/ota")
 public class OTAController {
 
-    private final String BIN_DIR = "C:/Summer_2026/Embedded/ESP32/Projects/Sentinel-OS/firmware_repo/Sentinel-OS.bin";
+
 
 
 }
