8b65decc8ba91d81fb0a6271e8a5451350d8b2b3
Ronaldo <ronaldoanthony12@gmail.com>
2026-08-03 17:16:08 -0400

    Calculate Sha-256

--- ota_backend/sentinel-api/src/main/java/com/sentinel/api/OTAService.java
+++ ota_backend/sentinel-api/src/main/java/com/sentinel/api/OTAService.java
@@ -1,10 +1,17 @@
 package com.sentinel.api;
 
 import jakarta.annotation.Resource;
+import org.apache.logging.log4j.message.Message;
 
 import java.io.File;
+import java.io.InputStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.security.MessageDigest;
+import java.util.HexFormat;
+
+import static org.springframework.security.config.http.MatcherType.path;
 
 public class OTAService {
 
@@ -15,7 +22,24 @@ public class OTAService {
         Path path = Paths.get(BIN_DIR + "sentinel_v" + ver + ".bin"); //path
         File bin_f = path.toFile(); //file obj from path. actual binary
 
+        if(!bin_f.exists()) throw new FileNotFoundException("Firmware binary not found.");
+    }
+
+    public String calc_sha256(Path path) throws Exception {
+        MessageDigest digest = MessageDigest.getInstance("SHA-256");
+
+        //opening an IS, and feeding it to sha
+        try(InputStream is = Files.newInputStream(path)) {
+            byte[] buf = new byte[8192]; //8kb per chunk
+            int bytes_read;
+            while((bytes_read = is.read(buf)) != -1) {
+                digest.update(buf, 0, bytes_read); //feeding to sha!
+            }
+        }
+        return HexFormat.of().formatHex(digest.digest());
 
     }
+
+
 
 }
