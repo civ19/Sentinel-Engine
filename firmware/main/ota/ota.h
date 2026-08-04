f521335a770a9058572450238c848430dd74a596
Ronaldo <ronaldoanthony12@gmail.com>
2026-08-04 13:45:23 -0400

    fix cmake again and includes on wifi.c

--- firmware/main/ota/ota.h
+++ firmware/main/ota/ota.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <stdint.h>
+
 extern const uint8_t server_cert_pem_start[] asm("_binary_sentinel_cert_pem_start");
 extern const uint8_t server_cert_pen_end[] asm("_binary_sentinel_cert_pem_end");
 
---
+++ firmware/main/wifi/wifi.c
@@ -0,0 +1,15 @@
+#include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+#include "esp_event.h"
+#include "nvs_flash.h"
+#include "esp_wifi.h" 
+#include "esp_netif.h" 
+#include "freertos/event_groups.h"
+
+#include "abstractions/abstractions.h"
+#include "wifi.h"
+
+static const char *TAG = "EVENT_LOOP";
+
+EventGroupHandle_t wifi_event_group = NULL;
+
