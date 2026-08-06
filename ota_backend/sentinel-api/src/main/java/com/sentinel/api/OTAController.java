92b6fa36ae4377b283ae6788f9d56a642ede482a
Ronaldo <ronaldoanthony12@gmail.com>
2026-08-06 10:41:30 -0400

    fix endpoint

--- ota_backend/sentinel-api/src/main/java/com/sentinel/api/OTAController.java
+++ ota_backend/sentinel-api/src/main/java/com/sentinel/api/OTAController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/ota")
+@RequestMapping("/api/ota")
 public class OTAController {
 
     private final OTAService service;
