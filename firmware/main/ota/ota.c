0a7235cb19a99c625204587dbe276ffd7ac6aee0
Ronaldo <ronaldoanthony12@gmail.com>
2026-08-06 10:31:00 -0400

    finish validating img header

--- firmware/main/ota/ota.c
+++ firmware/main/ota/ota.c
@@ -2,70 +2,39 @@
 #include "esp_log.h"
 #include "esp_http_client.h"
 #include "esp_https_ota.h"
-#include "cJSON.h"
 #include <string.h>
+#include <stdio.h>
 
 #include "abstractions/abstractions.h"
 
-#define MAX_RESP_BUF 512
+#define CURR_VER "1.0.0"
+#define SERVER_IP "172.17.35.33" //will provision this later via nimble
 
-static char resp_buf[MAX_RESP_BUF];
-static int resp_len = 0;
+
 static const char* TAG = "OTA";
 static const char* TAGS = "OTA Server";
 
 esp_http_client_handle_t client_handle;
 
-void manage_endpoints() {
-    mutex_log('I', TAG, "Attempting to sync firmware handshake data over HTTPS...");
 
-    esp_err_t initial_resp = esp_http_client_perform(client_handle); //acts as a courier. goes to our spring server, endpoints and all
-
-    if(initial_resp == ESP_OK) {
-        //if it connected to the server - wifi layer
-        mutex_log('I', TAGS, "TLS handshake with Server Successful!");
-        
-        int status_rc = esp_http_client_get_status_code(client_handle);
-        if(status_rc == 200) { 
-            mutex_log('I', TAGS, "Data Endpoint Fetch Successful! 200 OK.");
-        }
-        else mutex_log('E', TAGS, "Server REJECTED data! Status code: %d", status_rc);
-    } else mutex_log('I', TAGS, "Server handshake failed. %d", esp_err_to_name(initial_resp));
-
-
-}
-
-static esp_err_t _http_event_handler(esp_http_client_event_t *evt) {
-    switch(evt->event_id) { 
-        case HTTP_EVENT_ON_DATA:
-            if((resp_len + evt->data_len) < MAX_RESP_BUF) { //overflow check
-                memcpy((resp_buf + resp_len), evt->data, evt->data_len);
-                resp_len += evt->data_len;
-
-                resp_buf[resp_len] = "\0"; //null terminating end of the buf for safety
-            }
-
-            else mutex_log('E', TAGS, "Incoming JSON payload is too large for RAM Buffer.");
-        break;
-
-        case HTTP_EVENT_DISCONNECTED:
-            resp_len = 0; //preventing it from writing to ghost data if connection drops 
-        break;
-
-        default:
-        break;
+esp_err_t validate_img_header(esp_app_desc_t *new_app_info) {
+    if(new_app_info == NULL) {
+        return ESP_ERR_INVALID_ARG;
     }
 
-    return ESP_OK;
-}
+    esp_app_desc_t *app_desc = esp_app_get_description();
+    mutex_log('I', TAG, "Current app version: &s", app_desc->version);
+    mutex_log('I', TAG, "Updated(new) app version: %s", new_app_info->version);
 
+
+    return ESP_OK; //for anti rollback
+}
 
 void init_http_client(void) {
 
     esp_http_client_config_t http_conf = {
         .url = "https://172.17.35.33", //add nimBLE prov on this later
         .cert_pem = (const char *)server_cert_pem_start,
-        .event_handler = _http_event_handler,
         
     };
 
@@ -77,5 +46,10 @@ void init_http_client(void) {
     }
 
 }
+
+
+
+
+
 
 
