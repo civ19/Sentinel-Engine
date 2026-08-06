943223ee51596257737ce0252674fdc0b0f9ba75
Ronaldo <ronaldoanthony12@gmail.com>
2026-08-06 15:13:23 -0400

    Finish entire ota file att 1

--- firmware/main/ota/ota.c
+++ firmware/main/ota/ota.c
@@ -10,26 +10,24 @@
 
 #include "abstractions/abstractions.h"
 
-#define CURR_VER "1.0.0"
+
 #define SERVER_IP "172.17.35.33" //will provision this later via nimble
 #define OTA_URL_SIZE 256
 
 static const char* TAG = "OTA";
 static const char* TAGS = "OTA Server";
 
-static int sec_status_code = 0;
+static int status_code = 0;
 static char global_hash_header[65] = {0}; //sha256 hash buf
 
 
-bool isUpToDate(int status, esp_https_ota_handle_t handle) {
+void updated_check(esp_https_ota_handle_t handle, int status) {
     if(status == 304) {
         mutex_log('I', TAGS, "Backend returned 304: Firmware is already up to date.");
         esp_https_ota_abort(handle);
         vTaskDelete(NULL);
 
-        return true;
     }
-    return false;
 }
 
 esp_err_t validate_img_header(esp_app_desc_t *new_app_info) {
@@ -55,7 +53,7 @@ esp_err_t _http_event_handler(esp_http_client_event_t *evt) {
         break;
 
         case HTTP_EVENT_ON_FINISH:
-            sec_status_code = esp_http_client_get_status_code(evt->client);
+            status_code = esp_http_client_get_status_code(evt->client);
         break;
 
         default:
@@ -65,7 +63,7 @@ esp_err_t _http_event_handler(esp_http_client_event_t *evt) {
     return ESP_OK;
 }
 
-void init_ota(void) {
+esp_https_ota_handle_t init_ota(void) {
 
     const esp_app_desc_t* app_desc = esp_app_get_description(); //app desc
     char url[OTA_URL_SIZE];
@@ -87,18 +85,46 @@ void init_ota(void) {
     };
 
     esp_https_ota_handle_t update_handle = NULL;
-    esp_err_t ret = esp_https_ota_begin(&ota_conf, &ota_conf);
+    esp_err_t ret = esp_https_ota_begin(&ota_conf, &update_handle);
     if(ret != ESP_OK) {
         mutex_log('E', TAG, "ESP HTTPS OTA Begin failed (Check network connection or cert data)");
         vTaskDelete(NULL);
     }
+
+    return update_handle;
 
 }
 
 void perform_ota_task(void *pv) {
     mutex_log('I', TAG, "Starting OTA Update Task...");
     
+    status_code = 0;
+    memset(global_hash_header, 0, sizeof(global_hash_header));
 
+    esp_ota_handle_t ota_handle = init_ota();
+
+    updated_check(ota_handle, status_code);
+
+    if(strlen(global_hash_header) > 0) mutex_log('I', TAGS, "Server provided SHA256 checksum: %s", global_hash_header);
+
+    for(;;) { //streaming data chunks for downloading and flashing
+        esp_err_t ret = esp_https_ota_perform(ota_handle);
+        if(ret != ESP_ERR_HTTPS_OTA_IN_PROGRESS) break;
+    }
+
+    if(esp_https_ota_is_complete_data_received) {
+        esp_err_t ret = esp_https_ota_finish(ota_handle);
+        if(ret == ESP_OK) {
+            mutex_log('I', TAG, "Firmware Update Complete! Rebotting...");
+            esp_restart();
+        }
+        else mutex_log('E', TAG, "OTA Finish Failed. Return code: 0x%x", ret);
+    } else {
+        mutex_log('E', TAG, "OTA Data Stream Failed or connection timed out.");
+        esp_https_ota_abort(ota_handle);
+    }
+
+    vTaskDelete(NULL);
 
 
 }
