f7ee0c0466e5524ebe8f026b09950ff6d472b915
Ronaldo <ronaldoanthony12@gmail.com>
2026-08-07 07:51:51 -0400

    finish nvs increment cb

--- firmware/main/nvs_store/nvs_store.c
+++ firmware/main/nvs_store/nvs_store.c
@@ -6,6 +6,9 @@
 
 #include "abstractions/abstractions.h"
 
+const char* TAG = "NVS";
+
+void activate_safe_mode();
 
 void nvs_init(void) { //initializing nvs 
     esp_err_t ret = nvs_flash_init();
@@ -24,23 +27,32 @@ nvs_handle_t open_namespace(const char* name) { //making the debug namespace
     return nvs_h;
 }
 
-int32_t nvs_boot_track(nvs_handle_t nvs_h) {
-    int32_t boot = 0;
+int32_t nvs_increment_cb(nvs_handle_t nvs_h, const char *key, void (*on_error_cb)(void)) {
+    int32_t cnt = 0;
 
-    //read boot first. make a value in vs, store it.
+    //reading curr val:
+    esp_err_t ret = nvs_get_i32(nvs_h, key, &cnt);
+    if(ret == ESP_ERR_NOT_FOUND) cnt = 1;
+    else if(ret != ESP_OK) {
+        mutex_log('E',TAG, "NVS read failed for key: %s. Error: %s", key, esp_err_to_name(ret));
 
-    esp_err_t ret = nvs_get_i32(nvs_h, "boot_cnt", &boot);
-    if(ret == ESP_ERR_NVS_NOT_FOUND) boot = 1;
-    else { //if the value is there already
-        ESP_ERROR_CHECK(ret);
-        boot++;
+        if(on_error_cb != NULL) {
+            on_error_cb();
+        }
+        return -1;
     }
+    else cnt++; //if things go well and theres no read errors, we can just increment
 
-    ESP_ERROR_CHECK(nvs_set_i32(nvs_h, "boot_cnt", boot));
+    ret = nvs_set_i32(nvs_h, key, cnt);
+    if(ret != ESP_OK) mutex_log('E',TAG, "NVS write failed for key: %s. Error: %s", key, esp_err_to_name(ret));
 
     nvs_commit(nvs_h);
 
-    return boot;
+    return cnt;
+}
+
+int32_t nvs_key_track(nvs_handle_t nvs_h) {
+    //int32_t boot_cnt = nvs_increment_cb()
 }
 
 
