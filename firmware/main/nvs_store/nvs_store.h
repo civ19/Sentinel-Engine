1fbeb56251af1be3be91fa312bde672cddcc89e2
Ronaldo <ronaldoanthony12@gmail.com>
2026-08-08 21:10:06 -0400

    fix nvs store.h

--- firmware/main/nvs_store/nvs_store.h
+++ firmware/main/nvs_store/nvs_store.h
@@ -4,6 +4,7 @@
 #include "esp_err.h"
 #include <stdint.h>
 
+bool isBootLoop(void);
 esp_err_t init_nvs(void);
-esp_err_t nvs_reset(void);
+esp_err_t nvs_reset(bool cmd);
 int32_t nvs_increment_cb(const char* key, void (*on_error_cb)(void));
