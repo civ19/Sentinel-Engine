4c0391ac5d006ef349c77db371c9d7ae75ece4e0
Ronaldo <ronaldoanthony12@gmail.com>
2026-08-08 21:56:29 -0400

    finisb cmd_task.c

--- firmware/main/tasks/cmd_task.c
+++ firmware/main/tasks/cmd_task.c
@@ -5,3 +5,25 @@
 
 #include "abstractions/abstractions.h"
 
+void diagnostic_console_task(void* pv) {
+    printf("\n --- SENTINEL-OS SAFE MODE ---\n");
+    printf("Please type 'help' to see available commands.\n\n");
+
+    linenoiseSetPrompt("Sentinel> ");
+
+    for(;;) {
+        
+        char *line = limenoise(NULL);
+
+        if(line == NULL) continue;
+
+        int ret_cmd;
+
+        esp_err_t ret = esp_console_run(line, &ret_cmd);
+
+        if(ret == ESP_ERR_NOT_FOUND) printf("Unrecognized command. Type 'help' for more options.");
+        else if (ret == ESP_ERR_INVALID_ARG) printf("Empty command line.\n");
+
+        limenoiseFree(line);
+    }
+}
