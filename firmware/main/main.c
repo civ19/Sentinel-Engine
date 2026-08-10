d4dd431ae6d66d05aa39060c30ad3d48a9687e39
Ronaldo <ronaldoanthony12@gmail.com>
2026-08-10 18:41:37 -0400

    Fix main and finish entire v1 att 2

--- firmware/main/main.c
+++ firmware/main/main.c
@@ -60,19 +60,23 @@ void app_main(void) {
     esp_err_t ret = esp_ota_mark_app_valid_cancel_rollback(); 
     if(ret != ESP_OK)  mutex_log('E', TAG, "Failed to cancel rollback (Normal if running from factory slot). rc=%d", ret);
 
-    xTaskCreatePinnedToCore(wifi_connect_task, "wifiConnect", 4096, NULL, 5, &wifi_task_handle, 1);
-    xTaskCreatePinnedToCore(server_prov_task, "serverConnect", 4096, NULL, 5, &server_ip_handle, 1);
+    xTaskCreatePinnedToCore(wifi_connect_task, "wifiConnect", 4096, NULL, 3, &wifi_task_handle, 1);
+    xTaskCreatePinnedToCore(server_prov_task, "serverConnect", 4096, NULL, 3, &server_ip_handle, 1);
     if(ble_prov_task() != ESP_OK) {
         mutex_log('E', TAG, "Failed to init NimBLE Provisioning Stack.");
         return;
     }
 
     //gatekeeper
-    ESP_LOGI(TAG, "BLE active. Waiting for wifi BT provisioning...");
+    mutex_log('I', TAG, "BLE active. Waiting for wifi BT provisioning...");
     xEventGroupWaitBits(app_evt_group, WIFI_CONN_BIT, pdFALSE, pdTRUE, portMAX_DELAY);
-    ESP_LOGI(TAG, "Wifi set. Waiting for MQTT BT provisioning...");
+    mutex_log('I', TAG, "Wifi set. Waiting for MQTT BT provisioning...");
     xEventGroupWaitBits(app_evt_group, SVR_CONN_BIT, pdFALSE, pdTRUE, portMAX_DELAY);
-    ESP_LOGI(TAG, "We are online!");
+    mutex_log('I', TAG, "We are online!");
+
+    xTaskCreatePinnedToCore(perform_ota_task, "OtaUpdateTask", 8192, NULL, 2, NULL, 1); //finally runnign ota
+
+
  
 }
 
