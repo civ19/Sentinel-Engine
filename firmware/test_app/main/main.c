a97e21de756a7b20620c31fefb66f0674712882c
Ronaldo <ronaldoanthony12@gmail.com>
2026-08-12 10:02:53 -0400

    Testing fake data for assert

--- firmware/test_app/main/main.c
+++ firmware/test_app/main/main.c
@@ -1,23 +1,24 @@
-#include "unity.h"
+//#include "unity.h"
 #include "esp_task_wdt.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
+#include "test_ota.h"
+
 int sum(int a, int b) {
     return a+b;
 }
-
+/*
 TEST_CASE("Sum function works with positive ints", "[math]") 
 {
-    int result = sum(10,5);
+    int result = sum(60,60);
     TEST_ASSERT_EQUAL(15, result);
-}
+}*/
 
 void app_main(void) {
 
     
-    unity_run_menu(); 
-    
-    
+    process_data(NULL);
+
     
 }
--- firmware/test_app/main/test_ota.c
+++ firmware/test_app/main/test_ota.c
@@ -1,11 +1,5 @@
-#include "unity.h"
+#include <assert.h>
 
-int sum(int a, int b) {
-    return a+b;
-}
-
-TEST_CASE("Sum function works with positive ints", "[math]") 
-{
-    int result = sum(10,5);
-    TEST_ASSERT_EQUAL(15, result);
+void process_data(int* data) {
+    assert(data != NULL);
 }
