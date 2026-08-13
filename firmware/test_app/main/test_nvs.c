f8dc2ee9468b07fd92a7755544769f201f0cce5f
Ronaldo <ronaldoanthony12@gmail.com>
2026-08-13 13:37:13 -0400

    Sucessfully pass all test cases

--- firmware/test_app/main/test_nvs.c
+++ firmware/test_app/main/test_nvs.c
@@ -1,14 +1,15 @@
 #include "unity.h"
 #include "nvs_store/nvs_store.h"
 #include "nvs_flash.h"
+#include <inttypes.h>
+#include "esp_log.h"
 
-void setup(void) { //starting from a clean slate. kinda like @BeforeEach then cleaning the db in junit integratrion testing
+void setUp(void) { //starting from a clean slate. kinda like @BeforeEach then cleaning the db in junit integratrion testing
     esp_err_t ret;
 
     ret = nvs_flash_deinit();
-    TEST_ASSERT_EQUAL_INT32(ESP_OK, ret);
     ret = nvs_flash_erase();
-    TEST_ASSERT_EQUAL_INT32(ESP_OK, ret);
+
 
     ret = init_nvs();
     TEST_ASSERT_EQUAL_INT32(ESP_OK, ret);
@@ -16,17 +17,23 @@ void setup(void) { //starting from a clean slate. kinda like @BeforeEach then cl
     
 }
 
+void tearDown(void) 
+{
+    nvs_flash_deinit();
+}
+
+
 TEST_CASE("Boot loop detector returns TRUE after 3 sequential reboots, so we can run Safe mode", "[SAFE_MODE_TRIGGER]")
 {
-    setup();
+    
     for(int i = 0; i<3; i++) nvs_increment_cb("boot_count");
-
+    
     TEST_ASSERT_TRUE(isBootLoop());
 }
 
 TEST_CASE("Boot loop detector returns FALSE for LESS than 3 sequential reboots, so we can run Safe mode", "[SAFE_MODE_TRIGGER]")
 {
-    setup();
+    
     nvs_increment_cb("boot_count"); //this will only be like 2. so this fits the condition
 
     TEST_ASSERT_FALSE(isBootLoop());
@@ -34,10 +41,11 @@ TEST_CASE("Boot loop detector returns FALSE for LESS than 3 sequential reboots, 
 
 TEST_CASE("NVS Increment CB increments it by 1 everytime its called.", "[NVS]")
 {
-    setup();
+    
     int32_t mock_n1 = nvs_increment_cb("boot_count");
     int32_t mock_n2 = nvs_increment_cb("boot_count");
-
+    
+   
     TEST_ASSERT_EQUAL(1, mock_n1);
     TEST_ASSERT_EQUAL(2, mock_n2);
 
